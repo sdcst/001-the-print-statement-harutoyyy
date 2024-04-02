6ae25ac7646b9d2ad124934aa43cfd3c5d482e77
harutoyyy <haluomu0530@gmail.com>
2024-04-02 12:51:23 -0700

    s

--- assignment3.py
+++ assignment3.py
@@ -13,4 +13,5 @@ This is the first sentence.
 This is the second sentence.
 """
 
-print("This is the first sentence. This is the second sentence.")
+print("This is the first sentence.\n This is the second sentence.")
+
